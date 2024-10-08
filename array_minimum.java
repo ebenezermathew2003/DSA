7ce81ce25221d2000c412b5d9505c90d50884103
Ebenezer Mathew <ebenezermathew0711@gmail.com>
2024-10-08 19:21:06 +0530

    DSA

---
+++ array_minimum.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class array_minimum {
+    public static int minimum(int[] arr)
+    {
+        int min=arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]<min)
+            {
+                min=arr[i];
+            }
+        }
+        return min;
+    }
+ public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int[] arr=new int[n];
+    System.err.println("Elements are");
+    for(int i=0;i<n;i++)
+    {
+        arr[i]=sc.nextInt();
+    }
+    int res=minimum(arr);
+    System.err.println(res);
+
+ }   
+}
