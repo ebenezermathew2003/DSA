85ea22ee1d9ab0be93438bd432398f9401b7403a
Ebenezer Mathew <ebenezermathew0711@gmail.com>
2025-08-14 13:26:39 +0530

    practice

--- array_remove.java
+++ array_remove.java
@@ -12,7 +12,7 @@ class array_remove{
     {
         int OddCount=0;
         for(int i=0;i<arr.length;i++){
-            if(arr[i]!=0)
+            if(arr[i]%2!=0)
             {
                 OddCount++;
             }
