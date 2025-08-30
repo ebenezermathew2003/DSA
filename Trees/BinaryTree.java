e4c7fdca545bbc755eae3894ba013133c63a4aa9
Ebenezer Mathew <ebenezermathew0711@gmail.com>
2025-08-30 12:50:25 +0530

    practice

--- Trees/BinaryTree.java
+++ Trees/BinaryTree.java
@@ -42,12 +42,49 @@ public class BinaryTree {
         preOrder(root);
         System.out.println();
     }
+    public void inOrder(TreeNode root)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
+    public  void inOrderTraversal()
+    {
+        inOrder(root);
+        System.out.println();
+    }
+    public void postOrder(TreeNode root)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        postOrder(root.right);
+        postOrder(root.left);
+        System.out.print(root.data+" ");
+    }
+    public void postOrderTraversal()
+    {
+        postOrder(root);
+        System.out.println();
+    }
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
         bt.createBinaryTree();
 
-        System.out.print("Preorder Traversal");
+        System.out.print("Preorder Traversal  ");
         bt.preOrderTraversal();
+
+        System.out.print("InOrder Traversal  ");
+        bt.inOrderTraversal();
+
+        System.out.print("PostOrder Traversal  ");
+        bt.postOrderTraversal();
+
     }
     
 }
